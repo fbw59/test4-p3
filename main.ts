8fd8688edfd0e6b27005b39b9a54a95b03803eaf
fbw59 <154885509+fbw59@users.noreply.github.com>
2023-12-27 11:11:33 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,17 @@
 let SONAR = 0
-cuteBot.forward()
+cuteBot.motors(50, 50)
+basic.forever(function () {
+    basic.showIcon(IconNames.Ghost)
+    basic.showIcon(IconNames.Skull)
+})
 basic.forever(function () {
     SONAR = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
     if (SONAR > 2 && SONAR < 15) {
         cuteBot.motors(0, 0)
         basic.pause(500)
-        cuteBot.motors(randint(-50, -100), 0)
+        cuteBot.motors(randint(-50, -75), 0)
+        basic.pause(1000)
     } else {
-        basic.pause(2000)
-        cuteBot.forward()
+        cuteBot.motors(50, 50)
     }
 })
